e62d17e769449ca923ef597ce74bde51d011844a
ars0611 <cfky1251012@gn.iwasaki.ac.jp>
2026-03-01 02:42:04 +0900

    fix: 軽微な修正

--- src/lib/authService.ts
+++ src/lib/authService.ts
@@ -2,6 +2,7 @@ import type { GetAuthTokenResult } from "@/types/message";
 
 /**
  * 認証済みかチェック
+ * @remarks `chrome.identity.getAuthToken`は良い感じにキャッシュしてくれるから気軽に呼んで良い
  * @remarks 認証のUIは表示しない（{ interactive: false }）
  * @returns 認証済みでトークン取得出来たら true, それ以外 false
  */
@@ -9,13 +10,14 @@ export async function checkGoogleAuth(): Promise<boolean> {
     try {
         const result: GetAuthTokenResult = await chrome.identity.getAuthToken({ interactive: false });
         return !!result;
-    } catch {
+    } catch (e) {
         return false;
     }
 }
 
 /**
  * 認証済みかチェックし、認証済みでなかったら認証させる
+ * @remarks `chrome.identity.getAuthToken`は良い感じにキャッシュしてくれるから気軽に呼んで良い
  * @remarks 認証のUIを表示する（{ interactive: true }）
  * @returns 認証済みか、認証できたら true, それ以外 false
 */
@@ -23,13 +25,14 @@ export async function connectGoogleAuth(): Promise<boolean> {
     try {
         const result: GetAuthTokenResult = await chrome.identity.getAuthToken({ interactive: true });
         return !!result
-    } catch {
+    } catch (e) {
         return false;
     }
 }
 
 /**
  * 認証トークンを取得する
+ * @remarks `chrome.identity.getAuthToken`は良い感じにキャッシュしてくれるから気軽に呼んで良い
  * @returns access token
  * @throws エラー時に Error
  */
--- src/types/message.ts
+++ src/types/message.ts
@@ -1,6 +1,6 @@
 export type AuthCheckMessage = { type: "AUTH_CHECK" };
 export type AuthConnectMessage = { type: "AUTH_CONNECT" };
-export type LoadSheetMessage = { type: "LOAD_SHEET" };
+export type LoadSheetMessage = { type: "LOAD_SHEET", ranges: Array<string> };
 
 export type ExtensionMessage = AuthCheckMessage | AuthConnectMessage | LoadSheetMessage;
 
@@ -13,6 +13,6 @@ export type LoadSheetResponse = {
     ok: boolean,
     connected: boolean,
     error?: string,
-    sheetData: string | number[][],
+    sheetData: Array<Array<string | number>>,
 }
 export type GetAuthTokenResult = chrome.identity.GetAuthTokenResult;
